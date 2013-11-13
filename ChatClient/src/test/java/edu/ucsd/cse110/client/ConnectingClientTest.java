e49e15463f24c2b2c5cd82108d0a70a0bf79eeb8
Evan Carey <evanacarey@gmail.com>
2013-11-12 19:53:28 -0800

    Working on test cases.

---
+++ ChatClient/src/test/java/edu/ucsd/cse110/client/ConnectingClientTest.java
@@ -0,0 +1,27 @@
+package edu.ucsd.cse110.client;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConnectingClientTest {
+
+	ConnectingClient client;
+	
+	@Before
+	public void init() {
+		client = new ConnectingClient();
+	}
+	
+	@Test
+	public void testConnectingClient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOnMessage() {
+		fail("Not yet implemented");
+	}
+
+}
