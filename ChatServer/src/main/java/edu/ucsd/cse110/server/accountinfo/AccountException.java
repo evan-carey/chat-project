377ba4c3dcfdeebf98bad4365915a1947513800b
fabianlem <flemagut@ucsd.edu>
2013-11-26 17:37:37 -0800

    jhgjghjg

---
+++ ChatServer/src/main/java/edu/ucsd/cse110/server/accountinfo/AccountException.java
@@ -0,0 +1,31 @@
+package edu.ucsd.cse110.server.accountinfo;
+
+public class AccountException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public AccountException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
